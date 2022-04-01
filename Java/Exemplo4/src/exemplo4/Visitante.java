24da93492fc5b42316da1851a155548f680703c6
mcampagnoli1 <85307849+mcampagnoli1@users.noreply.github.com>
2022-04-01 10:18:44 -0300

    Add files via upload

---
+++ Java/Exemplo4/src/exemplo4/Visitante.java
@@ -0,0 +1,16 @@
+
+
+
+public class Visitante extends PessoaFisica {
+
+    public Visitante(String nome) {
+        super(nome);
+    }
+
+    public Visitante(String nome, String rg) {
+        super(nome, rg);
+    }
+    
+    
+    
+}
