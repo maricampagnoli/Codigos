e002a81be0adb386eee86aeed913b03a8ea78631
Mariana <85307849+maricampagnoli@users.noreply.github.com>
2022-05-19 09:14:04 -0300

    Add files via upload

---
+++ C#/Aula1/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula1.Models
+{
+    public class Contato
+    {
+        private int IdContato;
+        private string Nome;
+        private string Email;
+
+        public int IdContato { get => IdContato; set => IdContato = value; }
+        public string Nome { get => Nome; set => Nome = value; }
+        public string Email { get => Email; set => Email = value; }
+    }
+}
---
+++ C#/Aula1/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿create database BDContato
+go
+use BDContato
+go
+create table Contato (
+	IdContato int primary key identity,
+	Nome varchar(60) not null,
+	Email varchar(100) not null
+)
+insert into Contato values ('Fulano', 'fulano@gmail.com') 
+insert into Contato values ('Ciclano', 'ciclano@gmail.com')
+
+select * from Contato
